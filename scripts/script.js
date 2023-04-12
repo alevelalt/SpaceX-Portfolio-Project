20cb9a6793b2d95a8f805e3ca33f282e70c451e0
alevelalt <alevelalt@gmail.com>
2023-04-13 01:28:48 +0600

    Implimented hide header on scroll down feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const hamburgerBtn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
-
+const header =document.getElementById('header')
 
 hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.classList.toggle('hamburger-clicked');
@@ -8,6 +8,17 @@ hamburgerBtn.addEventListener('click', () => {
     document.body.classList.toggle('stop-scroll');
 });
 
+
+let lastScroll = 0;
+window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset;
+    if (currentScroll > lastScroll) {
+        header.classList.add('hide-header');
+    } else {
+        header.classList.remove('hide-header');
+    }
+    lastScroll = currentScroll;
+})
 
 
 
